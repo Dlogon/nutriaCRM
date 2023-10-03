e2b77bcff03be22ce7b478d99bceef81e7140fc6
Diego <dlogon@outlook.com>
2023-10-02 22:49:31 -0600

    import and export modules

---
+++ server/greet.js
@@ -0,0 +1,17 @@
+function sayHello(name){
+    return `Hello ${name}`
+}
+
+function goodBye(name){
+    return `Bye ${name}`
+}
+
+function goodMorning(name){
+    return `Hey Good morning ${name}`
+}
+
+module.exports = {
+    sayHello: sayHello,
+    goodBye : goodBye,
+    goodMorning : goodMorning
+}
