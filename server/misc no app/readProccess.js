5648577cb4f7a0c46a88a6bb9505cbb49ba61589
Diego <dlogon@outlook.com>
2023-10-05 17:39:50 -0600

    change array indexes

--- server/misc no app/readProccess.js
+++ server/misc no app/readProccess.js
@@ -1,6 +1,5 @@
 
 const programArgs = process.argv
-const number1 = 1 * programArgs[3];
-const number2 =  1 *programArgs[4];
-
+const number1 = 1 * programArgs[2];
+const number2 =  1 *programArgs[3];
 console.log(`sum ${number1} ${number2}`, number1 + number2)
