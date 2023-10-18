caaf4114cc67191128e420ad72e61155ac901c9a
Diego <dlogon@outlook.com>
2023-10-18 17:26:44 -0600

    basic node server

--- server/myVeryBasicServer.js
+++ server/myVeryBasicServer.js
@@ -1,12 +1,10 @@
-const http = require("http")
+const http = require("http");
 
 const PORT = 3000
 var myCounter = 0;
 const myServer = http.createServer((req, res) =>{
     myCounter ++;
-    console.log(req);
-    console.log(res);
-    res.end("Hello" + myCounter );
+    res.end("Hellos" + myCounter );
 })
 
 myServer.listen(PORT, ()=> console.log("Server is running"));
