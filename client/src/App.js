f06d2d0eb8cb31d7d5f71c897063342c7a76d5e0
Diego <dlogon@outlook.com>
2023-10-25 22:58:12 -0600

    add tailwind, add login form

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import React from "react";
+import LoginForm from './components/LoginForm/LoginForm'
 
 function App() {
 
   const [data, setData] = React.useState(null);
 
-  React.useEffect(()=> {
-    fetch("/api")
-    .then((res) => res.json())
-    .then((data) => setData(data.message))
-  })
+  // React.useEffect(()=> {
+  //   fetch("/api")
+  //   .then((res) => res.json())
+  //   .then((data) => setData(data.message))
+  //   .catch(err => setData(err.message) )
+  // })
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
+        <LoginForm />
       </header>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -5,16 +5,9 @@ const express = require("express");
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-const {data} = require("./mytestdata.json");
+const mainRouter = require("./routers/mainRouter")
 
-app.get("/", async (req, res) => {
-   res.json(data)    
-});
-
-app.get("/:id", async (req, res) => {
-  const id = req.params.id;
-  res.json(data.filter(d => d.id == id));    
-});
+app.use("/api", mainRouter)
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
